c49a94d3d23aec5d4aaf15e8461e9715cfce0451
noelia2016 <noeliacarrizo22@gmail.com>
2024-04-24 14:28:36 -0300

    Anda el descargar archivo, subir archivo y nuevo canvas, faltan los filtros y el lapiz

--- TP2_CarrizoNoelia/js/Main.js
+++ TP2_CarrizoNoelia/js/Main.js
@@ -2,52 +2,83 @@
 let canvas = document.querySelector('#canvas');
 let context = canvas.getContext('2d');
 
-let image;
-
 let canvasWidth = canvas.width;
 let canvasHeight = canvas.height;
 
-// para darle un color aleatorio a la figura
-function randomRGBA() {
-    let r = Math.round(Math.random() * 255);
-    let g = Math.round(Math.random() * 255);
-    let b = Math.round(Math.random() * 255);
-    let a = 255;
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
-}
+// tomo los elementos
+let archivo = document.querySelector("#newFile");
+let openFile = document.querySelector("#openFile");
+let downloadFile = document.querySelector("#downloadFile");
 
-// para borrar el canvas
-function clearCanvas(color, canvas) {
-    context.fillStyle = color;
+/** carga la imagen seleccionada en el canvas */
+function nuevoProyecto(){
+
+    context.fillStyle = "#fff"; // canvas background color
     context.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function cargar(){
-
-}
-
-function filtro(){
-
-}
-
-function iniciar() {
-
-    //Capturo el objeto del DOM donde indico si selecciono un punto interno de la figura
-  
-    var archivoSelect = document.getElementById("archivo");
-    var filtroSelect  = document.getElementById("filtro");
+    context.beginPath();
     
-    //Eventos que deben ocurrir cuando elijo filtro y cargo una imagen en el canvas
-    /*archivoSelect.onchange = function(e){
-        canvas.cargar(archivoSelect.value);
-    }
-    filtroSelect.onchange = function(e){
-        canvas.filtro(filtroSelect.value);
-    }
-    */
-    //Código
-
 }
 
-// inicio con un canvas vacio
-iniciar();
+// accion para descargar la imagen
+function downloadImage () {
+    let image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    downloadFile.setAttribute("href", image);
+}
+
+// abre un canvas vacio cuando presiona nuevo en el menu
+archivo.addEventListener("click", nuevoProyecto);
+
+// cuando hace click para cargar archivo
+openFile.addEventListener("change", e => {
+
+    // getting a hold of the file reference
+    let file = e.target.files[0];
+
+    // para quedarme y trabajar con el archivo utilizo FileReader
+
+    /** El objeto FileReader permite que las aplicaciones web lean ficheros (o información en buffer) almacenados en el cliente de forma asíncrona, usando los objetos File  */
+    let reader = new FileReader();
+    reader.readAsDataURL(file); // Comienza la lectura del contenido del objeto Blob, una vez terminada, el atributo result contiene un data: URL que representa los datos del fichero.
+    
+    // aquí le decimos al lector qué hacer cuando termine de leer....
+    reader.onload = readerEvent => {
+
+        let content = readerEvent.target.result; // me quedo con el contenido del archivo
+        image = new Image();
+        image.src = content;
+
+        // Este evento se activa cada vez que la operación de lectura se ha completado satisfactoriamente.
+        image.onload = function () {
+
+            // adapto la imagen a mi canvas
+            let imageAspectRatio = (1.0 * this.height) / this.width;
+            let imageScaledWidth;
+            let imageScaledHeight;
+
+            if (this.width > this.height) {
+                imageScaledWidth= canvas.height;
+                imageScaledHeight= canvas.height * imageAspectRatio;
+            }else{
+                imageScaledWidth= canvas.width;
+                imageScaledHeight= canvas.width * imageAspectRatio;
+            }
+
+            // muestro la imagen en el canvas
+            context.drawImage(this, 0, 0, imageScaledWidth, imageScaledHeight);
+
+            // obtener imageData del contenido del lienzo
+            let imageData = context.getImageData(0, 0, imageScaledWidth, imageScaledHeight);
+            
+            // dibuja la imagen modificada
+            context.putImageData(imageData, 0, 0);
+        }
+    }
+    // si no se cargo queda en null
+    openFile.value = null;
+    });
+
+//Evento al querer descargar la imagen que trabaje en el paint con filtros y demas
+downloadFile.addEventListener("click", downloadImage);
+
+
+
--- TP2_CarrizoNoelia/js/Pen.js
+++ TP2_CarrizoNoelia/js/Pen.js
@@ -1,18 +1,72 @@
 class Pen {
-    constructor(posX, posY, radius, fill, context) {
+    constructor(posX, posY, fill, context) {
         this.posX = posX;
         this.posY = posY;
         this.fill = fill;
         this.context = context;
     }
 
-    draw() {
-        this.context.fillStyle = this.fill;
-        
-        this.context.beginPath();
-        this.context.arc(this.posX, this.posY, this.radius, 0, 2 * Math.PI);
-        this.context.fill();
-        this.context.closePath();
+    /**
+     * Funcion que empieza a dibujar la linea
+     */
+    empezarDibujo() {
+
+        let posicion = canvas.getBoundingClientRect();
+        correccionX = posicion.x;
+        correccionY = posicion.y;
+        pintarLinea = true;
+        lineas.push([]);
+    };
+
+    /**
+     * Funcion que guarda la posicion de la nueva línea
+     */
+    guardarLinea() {
+
+        lineas[lineas.length - 1].push({
+            x: nuevaPosicionX,
+            y: nuevaPosicionY
+        });
+    }
+
+    /**
+     * Funcion dibuja la linea
+     */
+    dibujarLinea(event) {
+
+        event.preventDefault();
+        if (pintarLinea) {
+            // Estilos de linea
+            context.lineJoin = ctx.lineCap = 'round';
+            context.lineWidth = 10; // grosor de la linea
+            // Color de la linea
+            context.strokeStyle = '#fff';
+            // Marca el nuevo punto
+            if (event.changedTouches == undefined) {
+                // Versión ratón
+                nuevaPosicionX = event.layerX;
+                nuevaPosicionY = event.layerY;
+            } 
+            // Guarda la linea
+            guardarLinea();
+            // Redibuja todas las lineas guardadas
+            context.beginPath();
+            lineas.forEach(function (segmento) {
+                context.moveTo(segmento[0].x, segmento[0].y);
+                segmento.forEach(function (punto, index) {
+                    context.lineTo(punto.x, punto.y);
+                });
+            });
+            context.stroke();
+        }
+    }
+
+    /**
+     * Funcion que deja de dibujar la linea
+     */
+    pararDibujar () {
+        pintarLinea = false;
+        guardarLinea();
     }
 
 }
