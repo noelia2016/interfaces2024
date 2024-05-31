6eda6923ea1bacb352132a0a3292f55af3ab22ba
Noelia Carrizo <noeliacarrizo22@gmail.com>
2024-05-31 14:04:43 -0300

    Busco nuevos personajes y diseño de obstaculos

--- TP4_Carrizo_Noelia/js/Juego.js
+++ TP4_Carrizo_Noelia/js/Juego.js
@@ -17,6 +17,33 @@ let cantColisiones = 0;
 document.getElementById('puntos').textContent = "PUNTOS: " + puntos;
 document.getElementById('vidas').textContent = "VIDAS: " + vidas;
 
+
+// Función para actualizar el tiempo cada segundo
+function actualizarReloj() {
+    // Verifica si el tiempo ha llegado a cero
+    if (tiempo <= 0) {
+        clearInterval(intervalo);
+        gameOver();
+
+        return;
+    }
+
+    // Convierte el tiempo a minutos y segundos
+    let minutos = Math.floor(tiempo / 60);
+    let segundos = tiempo % 60;
+
+    // Formatea los valores para mostrarlos con dos dígitos
+    minutos = minutos < 10 ? '0' + minutos : minutos;
+    segundos = segundos < 10 ? '0' + segundos : segundos;
+
+    // Muestra en pantalla el contador en el elemento con el id "contador"
+    document.getElementById('contador').textContent = minutos + ':' + segundos;
+
+    // Reduce el tiempo en 1 segundo
+    tiempo--;
+
+}
+
 /** iniciar juego luego de presionar el boton jugar */
 function iniciarJuego(){
     
