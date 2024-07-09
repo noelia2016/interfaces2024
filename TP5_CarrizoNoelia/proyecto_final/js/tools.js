a9c7253106ca242a996c5dfa50f52fc26fd63528
noelia2016 <noeliacarrizo22@gmail.com>
2024-07-09 19:10:35 -0300

    Validacion de login y redirecciones correctas

--- TP5_CarrizoNoelia/proyecto_final/js/tools.js
+++ TP5_CarrizoNoelia/proyecto_final/js/tools.js
@@ -1,18 +1,28 @@
 
 var btn = document.getElementById('ingresar');
-var clave = document.getElementById('clave');
-var usuario = document.getElementById('usuario');
 
 btn.addEventListener('click', function(evt){
 
-    if (usuario.value != 'pepe' && clave.value != '1234') {
+    evt.preventDefault();
+    var clave = document.getElementById('clave');
+    var usuario = document.getElementById('usuario');
+
+    console.log(usuario.value);
+    console.log(clave.value);
+    if (usuario.value != 'pepe' || clave.value != '1234') {
+
+        // si no valido muestro este error 
         const nombreError = document.getElementById("nombreError");
         nombreError.classList.add("visible");
-        campoNombre.classList.add("invalido");
+        usuario.classList.add("invalido");
+        clave.classList.add("invalido");
+        usuario.value ='';
+        clave.value ='';
         nombreError.setAttribute("aria-hidden", false);
         nombreError.setAttribute("aria-invalid", true);
-    }else{
+    }else if (usuario.value == 'pepe' && clave.value == '1234'){
         // si valido redirecciono
+        console.log('valido el form redirecciona');
         window.location.href = 'conoce-las-mascotas.html';
     }
 
